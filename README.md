08c7f545c92729a6b937f5ff0a6b9fb784e90c84
John E.  Meador <johne.meador@johns-mbp-2.home>
2020-05-26 20:40:58 +0100

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # wales_food_network
 Repo for wales food network shiny app.
+
+For more information contact Dr Angelina Sanderson Bellamy at Cardiff University.
+
+
+
---
+++ shiny_deployment.R
@@ -0,0 +1,11 @@
+library(rsconnect)
+rsconnect::deployApp('path/to/your/app')
+
+
+# for connecting to shiny
+
+# rsconnect::setAccountInfo(
+#   name='elliotmeador84',
+#   token='1B1CDD3A7799AD4233FF8BD9C3B92275',
+#   secret='Jetj1c83qkfkh/wdTLDSDY/ju1Pz4Zv5DNgxZ9dO')
+
